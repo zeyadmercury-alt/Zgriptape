746fa31244a7387e069b7761d8ac3816d2de4317
mercuryvfxdev-art <mercuryvfxdev@gmail.com>
2025-12-21 22:50:24 +0200

    ediitng multi_image_generation_task to run in parallel

--- griptape/tasks/multi_image_generation_task.py
+++ griptape/tasks/multi_image_generation_task.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, List, Union
+from typing import Callable, Union
 
 from attrs import define, field
 from griptape.tasks import BaseTask
@@ -7,70 +7,40 @@ from griptape.artifacts import TextArtifact, ImageArtifact, ListArtifact
 from griptape.drivers.image_generation.openrouter import OpenRouterImageGenerationDriver
 
 
-
 @define
 class MultiImageGenerationTask(BaseTask):
-    """
-    A task that generates an image from a prompt and multiple reference images.
-    
-    Attributes:
-        image_generation_driver: The driver used to generate the image.
-        _input: Accepts either:
-            - tuple of (prompt string | TextArtifact, list[ImageArtifact])
-            - Callable returning ListArtifact [prompt, list of images]
-    """
-    image_generation_driver: OpenRouterImageGenerationDriver = field(
-        default=None,
-        kw_only=True
-    )
-    
+    image_generation_driver: OpenRouterImageGenerationDriver = field(kw_only=True)
+
     _input: Union[
         tuple[Union[str, TextArtifact], list[ImageArtifact]],
-        Callable[[BaseTask], ListArtifact]
+        Callable[[BaseTask], ListArtifact],
     ] = field(default=None, alias="input")
-    
+
     @property
     def input(self) -> ListArtifact:
-
-        if isinstance(self._input, ListArtifact):
-            return self._input
-            
-
         if isinstance(self._input, tuple):
-            prompt_str, images = self._input
-            prompt_artifact = TextArtifact(prompt_str) if isinstance(prompt_str, str) else prompt_str
-            
+            prompt, images = self._input
+            prompt_art = TextArtifact(prompt) if isinstance(prompt, str) else prompt
+            return ListArtifact([prompt_art, ListArtifact(images)])
 
-            if isinstance(images, list):
-                image_list_artifact = ListArtifact(images)
-            else:
-                image_list_artifact = ListArtifact([images])
-                
-            return ListArtifact([prompt_artifact, image_list_artifact])
-        
         if callable(self._input):
             return self._input(self)
-        
-        raise ValueError("Invalid input format")
 
-    @input.setter
-    def input(self, value: tuple[str | TextArtifact, list[ImageArtifact]] | Callable[[BaseTask], ListArtifact]):
-        self._input = value
+        raise ValueError("Invalid input")
+
+    async def run(self, *args) -> ImageArtifact:
+        """Async version for parallel execution."""
+        return self.try_run()
 
     def try_run(self) -> ImageArtifact:
         prompt_artifact: TextArtifact = self.input[0]
-
         images_artifact: ListArtifact = self.input[1]
-        images: list[ImageArtifact] = [img for img in images_artifact.value if isinstance(img, ImageArtifact)]
 
+        images = [img for img in images_artifact.value if isinstance(img, ImageArtifact)]
         if not images:
-            raise ValueError("All reference images must be ImageArtifact instances.")
+            raise ValueError("No valid ImageArtifact inputs")
 
-
-        output_image_artifact = self.image_generation_driver.run_multi_image_generation(
+        return self.image_generation_driver.run_multi_image_generation(
             prompts=[prompt_artifact.to_text()],
-            images=images
+            images=images,
         )
-
-        return output_image_artifact
-
